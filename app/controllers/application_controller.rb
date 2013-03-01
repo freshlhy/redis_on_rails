52e13efb9a550702b76fe310e627eebd933303fd
liuhuyu <735077491@qq.com>
2013-03-02 00:11:52 +0800

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
-  helper_method :attendee_signed_in?, :current_attendee
+
   protect_from_forgery
 
-  def current_attendee
-    Attendee.find_by_id(session[:attendee_id])
-  end
-
-  def current_attendee=(attendee)
-    session[:attendee_id] = attendee.to_param
-  end
 
 end
